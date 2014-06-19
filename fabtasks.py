5da9a22fd4d6ef9f744e9f825fd9d738959a927c
George Hamilton <george.hamilton@ed.ac.uk>
2014-06-19 14:04:19 +0100

    Issue #4 add fab task for generating sqlite db

--- fabtasks.py
+++ fabtasks.py
@@ -1,7 +1,4 @@
 import os
-#import geojson
-#import psycopg2
-#import sys
 
 from fabric.api import local, task
 from fabfile import _config
@@ -15,6 +12,20 @@ def convert_db_json():
     if os.path.exists(file_name):
         local('rm {0}'.format(file_name))
     cmd = 'ogr2ogr -f GeoJSON {0} "PG:host=localhost dbname={1} user={2} password={3} port={4}" -sql "select id, location from position_infos"'.format(
+        file_name,
+        _config('name', section='db'),
+        _config('user', section='db'),
+        _config('password', section='db'),
+        _config('port', section='db')
+    );
+    local(cmd)
+
+@task
+def convert_db_sqlite():
+    file_name = 'botanitours.sqlite'
+    if os.path.exists(file_name):
+        local('rm {0}'.format(file_name))
+    cmd = 'ogr2ogr -f SQLite -dsco SPATIALITE=yes {0} "PG:host=localhost dbname={1} user={2} password={3} port={4}" '.format(
         file_name,
         _config('name', section='db'),
         _config('user', section='db'),
