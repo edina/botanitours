9d021c6f1d5cb292581d92267c34aac1e25246e0
George Hamilton <george.hamilton@ed.ac.uk>
2014-06-20 15:44:41 +0100

    Issue #4 copy database to runtime android dir

--- fabtasks.py
+++ fabtasks.py
@@ -1,7 +1,38 @@
+"""
+Copyright (c) 2014, EDINA,
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice, this
+   list of conditions and the following disclaimer in the documentation and/or
+   other materials provided with the distribution.
+3. All advertising materials mentioning features or use of this software must
+   display the following acknowledgement: This product includes software
+   developed by the EDINA.
+4. Neither the name of the EDINA nor the names of its contributors may be used to
+   endorse or promote products derived from this software without specific prior
+   written permission.
+
+THIS SOFTWARE IS PROVIDED BY EDINA ''AS IS'' AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+SHALL EDINA BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+DAMAGE.
+"""
+
 import os
 
 from fabric.api import local, task
-from fabfile import _config
+from fabfile import _config, _get_source
 
 @task
 def convert_db_json():
@@ -22,7 +53,14 @@ def convert_db_json():
 
 @task
 def convert_db_sqlite():
-    file_name = 'botanitours.sqlite'
+    file_name = os.path.join(
+        _get_source()[1],
+        'platforms',
+        'android',
+        'assets',
+        'db',
+        'botanitours.sqlite')
+
     if os.path.exists(file_name):
         local('rm {0}'.format(file_name))
     cmd = 'ogr2ogr -f SQLite -dsco SPATIALITE=yes {0} "PG:host=localhost dbname={1} user={2} password={3} port={4}" '.format(
---
+++ platforms/android/src/uk/ac/edina/botanitours/ukacedinabotanitours.java
@@ -0,0 +1,72 @@
+/*
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+ */
+
+package uk.ac.edina.botanitours;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class ukacedinabotanitours extends CordovaActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        super.init();
+        super.loadUrl(Config.getStartUrl());
+
+        try{
+            File dbFile = getDatabasePath("botanitours.db");
+            if(!dbFile.exists()){
+                Log.v("info", "Database doesn't exist. Copy it.");
+                this.copy("botanitours.db", dbFile);
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    // copy db from assets to to database location
+    private void copy(String file, File dbFile) throws IOException{
+        String parentPath = dbFile.getParent();
+
+        File filedir = new File(parentPath);
+        if (!filedir.exists()) {
+            if (!filedir.mkdirs()) {
+                return;
+            }
+        }
+
+        InputStream in = this.getApplicationContext().getAssets().open("db/botanitours.sqlite");
+        //File newfile = new File(folder);
+        OutputStream out = new FileOutputStream(dbFile);
+
+        byte[] buf = new byte[1024];
+        int len; while ((len = in.read(buf)) > 0) out.write(buf, 0, len);
+        in.close(); out.close();
+    }
+}
